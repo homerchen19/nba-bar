f2d1db269a80780f71a20f88cc0616198b62e766
xxhomey19 <xxhomey19@gmail.com>
2018-10-04 11:19:55 +1000

    Use @containers instaed of relative path

--- renderer/reducers.js
+++ renderer/reducers.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 
-import homeReducer from './containers/Home/reducer';
-import previewReducer from './containers/Preview/reducer';
-import liveReducer from './containers/Live/reducer';
-import scoreboardReducer from './containers/Scoreboard/reducer';
-import standingsReducer from './containers/Standings/reducer';
-import settingsReducer from './containers/Settings/reducer';
+import homeReducer from '@containers/Home/reducer';
+import previewReducer from '@containers/Preview/reducer';
+import liveReducer from '@containers/Live/reducer';
+import scoreboardReducer from '@containers/Scoreboard/reducer';
+import standingsReducer from '@containers/Standings/reducer';
+import settingsReducer from '@containers/Settings/reducer';
 
 const rootReducer = combineReducers({
   home: homeReducer,
